e6c851665e2e1c9b61b33ef68e67f2c58d3d6e85
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2021-03-04 12:10:53 +0100

    2nd stage finished/not tested

--- test/ch/epfl/tchu/game/RouteTest.java
+++ test/ch/epfl/tchu/game/RouteTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.SortedBag;
 import org.junit.jupiter.api.Test;
 
 import ch.epfl.tchu.game.Route;
@@ -85,6 +86,17 @@ public class RouteTest {
     }
 
     @Test
+    /**
+     * BLACK(Color.BLACK),
+     *     VIOLET(Color.VIOLET),
+     *     BLUE(Color.BLUE),
+     *     GREEN(Color.GREEN),
+     *     YELLOW(Color.YELLOW),
+     *     ORANGE(Color.ORANGE),
+     *     RED(Color.RED),
+     *     WHITE(Color.WHITE),
+     *     LOCOMOTIVE(null);
+     */
     void possibleClaimCardsWorksFor4cases(){
         //You can use the list of all routes provided by creating this list and extracting
         //needed routes from it
@@ -93,10 +105,62 @@ public class RouteTest {
         List<Route> AllRoutes = ChMap.routes();
 
         //Route1 should correspond for case 1, route2 - case 2...
-        Route route1 = AllRoutes.get(0);
-        Route route2 = AllRoutes.get(0);
-        Route route3 = AllRoutes.get(0);
-        Route route4 = AllRoutes.get(0);
+        Route route1 = AllRoutes.get(3);//overground/not null/ violet length 2
+        Route route2 = AllRoutes.get(16);//overground/null/ length 4
+        Route route3 = AllRoutes.get(2);//underground/not null/ length 3 red
+        Route route4 = AllRoutes.get(0);//underground/null/ length 4
+
+        //test 1
+        SortedBag.Builder <Card> route1_ = new SortedBag.Builder<Card>();
+        route1_.add(SortedBag.of(2, Card.VIOLET));
+        List <SortedBag <Card>> route1_answer = List.of(route1_.build());
+
+        for (SortedBag <Card> a : route1_answer){
+            System.out.println(a);
+        }
+
+        for (SortedBag <Card> a : route1.possibleClaimCards()){
+            System.out.println(a);
+        }
+
+        assertEquals(route1_answer, route1.possibleClaimCards());
+
+        //test 2
+        SortedBag.Builder <Card> route2_ = new SortedBag.Builder<Card>();
+        route2_.add(SortedBag.of(4, Card.BLACK));
+        route2_.add(SortedBag.of(4, Card.VIOLET));
+        route2_.add(SortedBag.of(4, Card.BLUE));
+        route2_.add(SortedBag.of(4, Card.GREEN));
+        route2_.add(SortedBag.of(4, Card.YELLOW));
+        route2_.add(SortedBag.of(4, Card.ORANGE));
+        route2_.add(SortedBag.of(4, Card.RED));
+        route2_.add(SortedBag.of(4, Card.WHITE));
+        List <SortedBag <Card>> route2_answer = List.of(route2_.build());
+        assertEquals(route2_answer, route2.possibleClaimCards());
+
+        for (SortedBag <Card> a : route2_answer){
+            System.out.println(a);
+        }
+
+        for (SortedBag <Card> a : route2.possibleClaimCards()){
+            System.out.println(a);
+        }
+
+
+        //test 3
+        SortedBag.Builder <Card> route3_ = new SortedBag.Builder<Card>();
+        route3_.add(SortedBag.of(3, Card.RED));
+        route3_.add(SortedBag.of(2, Card.RED, 1, Card.LOCOMOTIVE));
+        route3_.add(SortedBag.of(1, Card.RED, 2, Card.LOCOMOTIVE));
+        route3_.add(SortedBag.of(3, Card.LOCOMOTIVE));
+
+        List <SortedBag <Card>> route3_answer = List.of(route3_.build());
+        assertEquals(route3_answer, route3.possibleClaimCards());
+
+
+
+
+
     }
 
 
