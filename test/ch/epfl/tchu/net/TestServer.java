bf50bcfc61d62aa2b39d3086be9df877901b61a7
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2021-04-28 21:12:31 +0200

    8.2 update

--- test/ch/epfl/tchu/net/TestServer.java
+++ test/ch/epfl/tchu/net/TestServer.java
@@ -9,14 +9,19 @@ import java.net.Socket;
 import java.util.Map;
 
 public final class TestServer {
+
     public static void main (String [] args) throws IOException {
-        System.out.println ( "Starting server!" );
-        try (ServerSocket serverSocket = new ServerSocket ( 5108 );
-        Socket socket = serverSocket.accept ()) {
-            Player playerProxy = new RemotePlayerProxy (socket);
-            var playerNames = Map.of (PlayerId.PLAYER_1, "Ada", PlayerId.PLAYER_2, "Charles" );
-            playerProxy.initPlayers (PlayerId.PLAYER_1, playerNames);
+
+        System.out.println("Starting server!");
+
+        try (ServerSocket serverSocket = new ServerSocket(5108);
+
+        Socket socket = serverSocket.accept()){
+            Player playerProxy = new RemotePlayerProxy(socket);
+            var playerNames = Map.of(PlayerId.PLAYER_1, "Ada", PlayerId.PLAYER_2, "Charles");
+            playerProxy.initPlayers(PlayerId.PLAYER_1, playerNames);
         }
-        System.out.println ( "Server done!" );
+
+        System.out.println("Server done!");
     }
 }
