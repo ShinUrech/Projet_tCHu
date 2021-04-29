b8be4c36f59b27e56fd76d4ba0e81967de1f7a21
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2021-04-29 21:34:10 +0200

    stage 8 finished

--- src/ch/epfl/tchu/net/RemotePlayerClient.java
+++ src/ch/epfl/tchu/net/RemotePlayerClient.java
@@ -1,7 +1,8 @@
 package ch.epfl.tchu.net;
 
-import ch.epfl.tchu.game.Player;
-import ch.epfl.tchu.game.PlayerId;
+import ch.epfl.tchu.Preconditions;
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
 
 import java.io.*;
 import java.net.Socket;
@@ -12,42 +13,158 @@ import java.util.regex.Pattern;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
+/**
+ * A class that represents a remote Client.
+ *
+ * @author Aidas Venckunas (325464)
+ */
 public class RemotePlayerClient {
 
-    private Player player;
-    private String hostName;
-    private int port;
+    private final Player player;
+    private final String hostName;
+    private final int port;
 
-    public  RemotePlayerClient(Player player, String hostName, int port){
+    /**
+     * A public constructor for a remote Client.
+     *
+     * @param player a remote player (player that is running on a different program)
+     * @param hostName a name of the hosting server
+     * @param port a port that a server is attached to
+     * @throws IllegalArgumentException if a given player is empty or if a hostName is empty
+     */
+    public RemotePlayerClient(Player player, String hostName, int port){
+
+        Preconditions.checkArgument(player != null && hostName != null);
+
         this.player = player;
         this.hostName = hostName;
         this.port = port;
     }
 
+    /**
+     * A method that runs all the processes between a proxy and a client:
+     * reads a message from a proxy,
+     * deserializes it,
+     * calls a requested method for the client player,
+     * serializes and returns the needed information back to the proxy.
+     */
     public void run(){
         try (Socket s = new Socket(hostName, port);
-             BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream(), US_ASCII));
-             BufferedWriter w = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), US_ASCII))){
 
-             String message = r.readLine();
-             String data[] = message.split(Pattern.quote(" "), -1);
-             MessageId messageType = MessageId.valueOf(data[0]);
+            BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream(), US_ASCII))){
 
-             switch(messageType) {
+            String message;
+            do{
+                message = r.readLine();
+                if(message == null){
+                    break;
+                }
 
-                 case INIT_PLAYERS:
+                String[] data = message.split(Pattern.quote(" "), -1);
+                MessageId messageType = MessageId.valueOf(data[0]);
 
-                     PlayerId ownId = Serdes.PLAYER_ID_SERDE.deserialize(data[1]);
-                     List<String> namesList = Serdes.STRING_LIST_SERDE.deserialize(data[2]);
-                     Map<PlayerId, String> names = new EnumMap<PlayerId, String>(PlayerId.class);
+                switch (messageType) {
 
-                     names.put(PlayerId.PLAYER_1, namesList.get(0));
-                     names.put(PlayerId.PLAYER_2, namesList.get(1));
+                    case INIT_PLAYERS:
 
-                     player.initPlayers(ownId, names);
-             }
-        } catch (IOException e) {
-            throw  new UncheckedIOException (e);
+                         PlayerId ownId = Serdes.PLAYER_ID_SERDE.deserialize(data[1]);
+                         List<String> namesList = Serdes.STRING_LIST_SERDE.deserialize(data[2]);
+                         Map<PlayerId, String> names = new EnumMap<PlayerId, String>(PlayerId.class);
+
+                         names.put(PlayerId.PLAYER_1, namesList.get(0));
+                         names.put(PlayerId.PLAYER_2, namesList.get(1));
+
+                         player.initPlayers(ownId, names);
+                         break;
+
+                     case RECEIVE_INFO:
+
+                         String info = Serdes.STRING_SERDE.deserialize(data[1]);
+
+                         player.receiveInfo(info);
+                         break;
+
+                     case UPDATE_STATE:
+
+                         PublicGameState newState = Serdes.PUBLIC_GAME_STATE_SERDE.deserialize(data[1]);
+                         PlayerState ownState = Serdes.PLAYER_STATE_SERDE.deserialize(data[2]);
+
+                         player.updateState(newState, ownState);
+                         break;
+
+                     case SET_INITIAL_TICKETS:
+
+                         SortedBag<Ticket> tickets = Serdes.TICKET_SORTEDBAG_SERDE.deserialize(data[1]);
+
+                         player.setInitialTicketChoice(tickets);
+                         break;
+
+                     case CHOOSE_INITIAL_TICKETS:
+
+                         SortedBag<Ticket> chosenInitialTickets = player.chooseInitialTickets();
+
+                         send(Serdes.TICKET_SORTEDBAG_SERDE.serialize(chosenInitialTickets), s);
+                         break;
+
+                     case NEXT_TURN:
+
+                         Player.TurnKind nextTurn = player.nextTurn();
+
+                         send(Serdes.TURN_KIND_SERDE.serialize(nextTurn), s);
+                         break;
+
+                     case CHOOSE_TICKETS:
+
+                         SortedBag<Ticket> options = Serdes.TICKET_SORTEDBAG_SERDE.deserialize(data[1]);
+                         SortedBag<Ticket> chosenTickets = player.chooseTickets(options);
+
+                         send(Serdes.TICKET_SORTEDBAG_SERDE.serialize(chosenTickets), s);
+                         break;
+
+                     case DRAW_SLOT:
+
+                         int drawSlot = player.drawSlot();
+
+                         send(Serdes.INTEGER_SERDE.serialize(drawSlot), s);
+                         break;
+
+                     case ROUTE:
+
+                         Route claimedRoute = player.claimedRoute();
+
+                         send(Serdes.ROUTE_SERDE.serialize(claimedRoute), s);
+                         break;
+
+                     case CARDS:
+
+                         SortedBag<Card> initialCards = player.initialClaimCards();
+
+                         send(Serdes.CARD_SORTEDBAG_SERDE.serialize(initialCards), s);
+                         break;
+
+                     case CHOOSE_ADDITIONAL_CARDS:
+
+                         List<SortedBag<Card>> optionsCards = Serdes.CARD_SORTEDBAG_LIST_SERDE.deserialize(data[1]);
+
+                         SortedBag<Card> chosenCards = player.chooseAdditionalCards(optionsCards);
+
+                         send(Serdes.CARD_SORTEDBAG_SERDE.serialize(chosenCards), s);
+                         break;
+                }
+            }while(message != null);
+        }catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void send(String message, Socket socket){
+        try{
+            BufferedWriter w = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), US_ASCII));
+            w.write(message);
+            w.write('\n');
+            w.flush();
+        }catch(IOException e){
+            throw new UncheckedIOException(e);
         }
     }
 }
--- test/ch/epfl/tchu/net/TestClient.java
+++ test/ch/epfl/tchu/net/TestClient.java
@@ -3,11 +3,13 @@ package ch.epfl.tchu.net;
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import ch.epfl.tchu.game.ChMap;
 
 public final class TestClient {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         System.out.println("Starting client!");
         RemotePlayerClient playerClient = new RemotePlayerClient(new TestPlayer(), "localhost", 5108);
         playerClient.run();
@@ -20,58 +22,105 @@ public final class TestClient {
 
             System.out.printf("ownId:%s \n", ownId);
             System.out.printf("playerNames:%s \n", names);
+            System.out.println();
         }
 
         @Override
         public void receiveInfo(String info) {
-
+            System.out.println("info received: ");
+            System.out.println(info);
+            System.out.println();
         }
 
         @Override
         public void updateState(PublicGameState newState, PlayerState ownState) {
-
+            System.out.println("State has been updated: ");
+            System.out.printf("PlayerState: %s", ownState.toString());
+            System.out.println();
+            System.out.printf("New game state: %s \n", newState.toString());
+            System.out.println();
         }
 
         @Override
         public void setInitialTicketChoice(SortedBag<Ticket> tickets) {
-
+            System.out.println("Choose initial tickets from: ");
+            System.out.println(tickets);
+            System.out.println();
         }
 
         @Override
         public SortedBag<Ticket> chooseInitialTickets() {
-            return null;
+            System.out.println("Player has chosen these initial tickets: ");
+
+            SortedBag.Builder<Ticket> builder = new SortedBag.Builder<>();
+            builder.add(1, ChMap.tickets().get(0));
+            builder.add(1, ChMap.tickets().get(1));
+            builder.add(1, ChMap.tickets().get(2));
+
+            SortedBag<Ticket> initialTickets = builder.build();
+            System.out.println(initialTickets);
+            System.out.println();
+            return initialTickets;
         }
 
         @Override
         public TurnKind nextTurn() {
-            return null;
+            System.out.println("Player chooses next turn to be: ");
+            System.out.println(TurnKind.DRAW_CARDS.name());
+            System.out.println();
+            return TurnKind.DRAW_CARDS;
         }
 
         @Override
         public SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options) {
-            return null;
+            System.out.println("Player has to choose tickets from: ");
+            System.out.println(options);
+            System.out.println("Player has chosen: ");
+
+            SortedBag.Builder<Ticket> builder = new SortedBag.Builder<>();
+            builder.add(1, ChMap.tickets().get(3));
+            builder.add(1, ChMap.tickets().get(4));
+            builder.add(1, ChMap.tickets().get(5));
+
+            SortedBag<Ticket> tickets = builder.build();
+
+            System.out.println(tickets);
+            System.out.println();
+
+            return tickets;
         }
 
         @Override
         public int drawSlot() {
-            return 0;
+            System.out.printf("Player has chosen to draw car no. %s \n", 2);
+            System.out.println();
+            return 2;
         }
 
         @Override
         public Route claimedRoute() {
-            return null;
+            System.out.printf("Player has claimed a route %s \n", ChMap.routes().get(1));
+            System.out.println();
+            return ChMap.routes().get(1);
         }
 
         @Override
         public SortedBag<Card> initialClaimCards() {
-            return null;
+            System.out.println("Player has used these cards to claim a route: ");
+            System.out.println(SortedBag.of(2, Card.YELLOW, 1, Card.BLUE));
+            System.out.println();
+            return SortedBag.of(2, Card.YELLOW, 1, Card.BLUE);
         }
 
         @Override
         public SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options) {
-            return null;
+            System.out.println("Player has to choose additional cards from: ");
+            System.out.println(options);
+            System.out.println("Player has chosen: ");
+            System.out.println(SortedBag.of(2, Card.YELLOW, 1, Card.BLUE));
+            System.out.println();
+            return SortedBag.of(2, Card.YELLOW, 1, Card.BLUE);
         }
 
-        //… other Player methods
     }
 }
