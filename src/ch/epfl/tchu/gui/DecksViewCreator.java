58fd6b3426406d1ec7538b3b6a350b868e8939ca
urech <shin.urech@epfl.ch>
2021-05-06 12:14:15 +0200

    1.9.1

---
+++ src/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -0,0 +1,13 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.game.PublicCardState;
+
+public class DecksViewCreator {
+
+    private DecksViewCreator(){};
+
+    public void creatHandView(PublicCardState currentState){
+
+    }
+
+}
